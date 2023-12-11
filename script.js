809b8c21a1465fcc5ef25824d9200b4742f61d34
Logan King <59426296+EagleAim@users.noreply.github.com>
2023-12-10 22:10:00 -0800

    Answered questions.

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("The repositories that were published after every class helped alot especialy getting something to build around.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("I do not have any suggestions to change the course");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I learned what I expected to go into this course.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("The entirety of javaScript was new to me, the only thing that wasnt as new was setting up buttons and other html elements.");
